281978323455ca56da27c58e557e33318b5934d3
mostafakhalili <github123@#>
2020-07-26 17:57:11 +0430

    adding Dialog sample and some comments

--- patterns/src/main/java/abstractfactory/gui/GUIFactory.java
+++ patterns/src/main/java/abstractfactory/gui/GUIFactory.java
@@ -1,5 +1,6 @@
 package abstractfactory.gui;
 
+//Abstract Factory is a creational design pattern that lets you produce families of related objects without specifying their concrete classes.
 public interface GUIFactory {
 
     public Button createButton();
